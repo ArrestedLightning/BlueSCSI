39df7d334cb9d33d050eea8599630527df1325f1
ArrestedLightning <10822331+ArrestedLightning@users.noreply.github.com>
2022-09-05 15:28:43 -0500

    Added note on usage to merge script

--- merge_bootloader.sh
+++ merge_bootloader.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#First build the STM32F1-bootloader configuration, then run this script to merge in the USB bootloader image
 srec_cat usb_bootloader.hex -intel .pio/build/STM32F1-bootloader/firmware.hex -intel -o firmware_with_bootloader.hex -intel
